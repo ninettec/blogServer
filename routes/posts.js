bbf2713f31ab63c210abdd540dd9571a70b80d00
Ninette Calderon <ninettecalderon@Ninettes-MacBook-Pro-4.local>
2022-07-07 15:01:07 -0700

    final push

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,7 @@ router.post("/", async (req, res) => {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
 });
